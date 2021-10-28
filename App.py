79f92933e5817e8d9c3af5e10410f0739e0e1982
sandeep kirwai <sandeepkirwai@gmail.com>
2021-10-28 08:37:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -94,46 +94,46 @@ if file is not None:
             y = dataset['Label']
             x = dataset[names]
 
-            st.subheader('As you can see from the dataset. It is having negative values as well. :')
-            st.subheader('Also dataset contains -999 values which is less relevent so which technique you want to use to imputing')
-            mopt = st.selectbox("Select :", ("convert to zero", "convert to mean"))
-            # "Click to select",
-            if (st.button("START imputing")):
-                    if "convert to zero" in mopt:
+    st.subheader('As you can see from the dataset. It is having negative values as well. :')
+    st.subheader('Also dataset contains -999 values which is less relevent so which technique you want to use to imputing')
+    mopt = st.selectbox("Select :", ("convert to zero", "convert to mean"))
+    # "Click to select",
+        if (st.button("START imputing")):
+            if "convert to zero" in mopt:
 
-                        imp_zero = SimpleImputer(missing_values=-999.0, strategy='constant', fill_value=0)
+                imp_zero = SimpleImputer(missing_values=-999.0, strategy='constant', fill_value=0)
+                # Imputation transformer for completing missing values.
+                imp_zero.fit(x)
+                dataset = imp_zero.transform(x)
+            if "convert to mean" in mopt:
+                imp_mean = SimpleImputer(missing_values=-999.0, strategy='mean')
                         # Imputation transformer for completing missing values.
-                        imp_zero.fit(x)
-                        dataset = imp_zero.transform(x)
-                    if "convert to mean" in mopt:
-                        imp_mean = SimpleImputer(missing_values=-999.0, strategy='mean')
-                        # Imputation transformer for completing missing values.
-                        imp_mean.fit(x)
-                        dataset = imp_mean.transform(x)
+                imp_mean.fit(x)
+                dataset = imp_mean.transform(x)
                     
-                    st.write(dataset)
+            st.write(dataset)
 
-                    st.subheader('As we know that data has skewness so need to scale numeric columns ')
-                    st.subheader('Which technique you want to use ')
+    st.subheader('As we know that data has skewness so need to scale numeric columns ')
+    st.subheader('Which technique you want to use ')
 
-                    scalopt = st.selectbox("Select :", ("StandardScaler", "Normalize"))
-                    # "Click to select",
-                    if (st.button("START scalling")):
-                          if "StandardScaler" in scalopt:
-                             from sklearn.preprocessing import StandardScaler
-                             scaler = StandardScaler()
-                             x = scaler.fit_transform(dataset)
-                          if "Normalize" in scalopt:
-                             from sklearn.preprocessing import normalize
-                             x = normalize(dataset)
+    scalopt = st.selectbox("Select :", ("StandardScaler", "Normalize"))
+    # "Click to select",
+    if (st.button("START scalling")):
+        if "StandardScaler" in scalopt:
+            from sklearn.preprocessing import StandardScaler
+            scaler = StandardScaler()
+            x = scaler.fit_transform(dataset)
+        if "Normalize" in scalopt:
+            from sklearn.preprocessing import normalize
+            x = normalize(dataset)
                     
-                    st.write(x)
-                    st.subheader('Test size split of users choice:')
-                    st.text('Default is set to 20%')
-                    splt = st.number_input('', step=5, min_value=10, value=20)
-                    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=splt * 0.01, random_state=0)
-                    st.write("Data is being split into testing and training data!")
+        st.write(x)
+    st.subheader('Test size split of users choice:')
+    st.text('Default is set to 20%')
+    splt = st.number_input('', step=5, min_value=10, value=20)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=splt * 0.01, random_state=0)
+    st.write("Data is being split into testing and training data!")
                     # Splitting the data into 20% test and 80% training data
                     # Outlier detection and removal
-                    st.success("Data cleaned!")
+    st.success("Data cleaned!")
 
